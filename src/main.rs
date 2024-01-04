2d8d7b09a89d0ab41cdcaf8328d98610c4a575a3
tasogare66 <tasogare66@gmail.com>
2024-01-04 22:11:08 +0900

    Added tasks.json

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ fn main() {
         .add_plugins(DefaultPlugins.set(WindowPlugin {
             primary_window: Some(Window {
                 title: "bevyruma".into(),
-                resolution: (1024f32, 720f32).into(),
+                resolution: (1280f32, 720f32).into(),
                 ..Default::default()
             }),
             ..Default::default()
