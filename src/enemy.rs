e5f956d7404d39e2ac7696be9cf7735609b08dbe
tasogare66 <tasogare66@gmail.com>
2024-01-08 00:00:32 +0900

    Added enemy_movement_system

--- src/enemy.rs
+++ src/enemy.rs
@@ -18,12 +18,14 @@ pub struct EnemyPlugin;
 
 impl Plugin for EnemyPlugin {
     fn build(&self, app: &mut App) {
-        app.insert_resource(EnemyCount { ..default() }).add_systems(
-            Update,
-            enemy_spawn_system
-                .run_if(on_timer(Duration::from_secs_f32(2. / 60.)))
-                .in_set(GameSystemSet::PostUpdate),
-        );
+        app.insert_resource(EnemyCount { ..default() })
+            .add_systems(Update, enemy_movement_system.in_set(GameSystemSet::Update))
+            .add_systems(
+                Update,
+                enemy_spawn_system
+                    .run_if(on_timer(Duration::from_secs_f32(2. / 60.)))
+                    .in_set(GameSystemSet::PostUpdate),
+            );
     }
 }
 
@@ -66,5 +68,21 @@ fn enemy_spawn_system(
             .insert(PhysicalObj { ..default() });
 
         enemy_count.count += 1;
+    }
+}
+
+fn enemy_movement_system(
+    q_player: Query<&Transform, With<Player>>,
+    time: Res<Time>,
+    mut q_enemy: Query<(&Transform, &mut PhysicalObj), With<Enemy>>,
+) {
+    // playerに近づく
+    let Ok(pl_tf) = q_player.get_single() else {
+        return;
+    };
+    let dt = time.delta_seconds();
+    for (ene_tf, mut obj) in q_enemy.iter_mut() {
+        let dir = (pl_tf.translation.xy() - ene_tf.translation.xy()).normalize_or_zero();
+        obj.move_vec += dir * 18. * dt;
     }
 }
